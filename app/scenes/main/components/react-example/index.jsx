05a10e5ff885435e98eb76aa9912aa847cb1efce
JoshSpears3 <josh.spears@sq1.com>
2017-07-24 09:04:03 -0500

    Adding redux, also updated example message

--- app/scenes/main/components/react-example/index.jsx
+++ app/scenes/main/components/react-example/index.jsx
@@ -30,7 +30,7 @@ class MainScene extends React.Component{
       <div className="react-example">
         <h1>Event Bubbling</h1>
         <h3>React VS Redux</h3>
-        <p>Passing data from one component to another in an React application can be very tedious to create and hard to maintain based on the size of the application. Triggering an event to pass data from the child component all the way up to parent, passing data down from the parent to the child components, or sequentially depending on the requirements of your application. To fully understand the difference in this native react behavior compared to redux, I decided to create a simple example that updates background colors of components when an event is triggered from the lowest component possible.</p>
+        <p>Passing data from one component to another in an React application can be very tedious to create and hard to maintain based on the size of the application. Triggering an event to pass data from the child component all the way up to parent, passing data down from the parent to the child components, or sequentially depending on the requirements of your application. To fully understand the differences in the native react behavior for event bubbling compared to redux, I decided to create simple examples that update background colors of the components when an event is triggered from the lowest component possible.</p>
         <ComponentThree onChange={this.handleChange}/>
         <ComponentFour styles={this.state.styles}/>
         <img src="app/assets/images/react-example.png" alt=""/>
